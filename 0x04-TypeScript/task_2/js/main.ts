1889cf10cec27ffdcf74d61164359ca37e0be8bd
maureen-qui <maureenwangui08@yahoo.com>
2023-10-06 08:00:02 +0000

    creating functions specific to employee

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,71 @@
+// DirectorInterface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// TeacherInterface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Director class implementing DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+// Teacher class implementing TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// Function createEmployee
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Function isDirector
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Function executeWork
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Example usage
+console.log(executeWork(createEmployee(200))); // Should return an instance of Teacher
+console.log(executeWork(createEmployee(1000))); // Should return an instance of Director
+console.log(executeWork(createEmployee('$500'))); // Should return an instance of Director
